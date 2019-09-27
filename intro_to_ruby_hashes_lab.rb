1d0ad0d9735e4262049b2909d96ddb6120cddcce
Diego Martinez <dmartinezm@gmail.com>
2019-09-27 03:50:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,8 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  crea={key=>value}
+  crea
 end
 
 def read_from_hash(hash, key)
